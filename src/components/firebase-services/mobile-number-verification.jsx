40e3861b15cf980b968706ce1fe80a08f3fc8a8b
prakhar7s <prakharshrivastava971@gmail.com>
2021-06-16 23:13:10 +0530

    otp verfication component code improved and is working  much better then the earlier one

--- src/components/firebase-services/mobile-number-verification.jsx
+++ src/components/firebase-services/mobile-number-verification.jsx
@@ -5,11 +5,41 @@ import "./mobile-number-verification.css";
 
 const MobileNumberVerification = () => {
   const [confirmationResult, setConfirmationResult] = useState(null);
-  const verifyButtonRef = useRef();
+  const [isLoading, setLoading] = useState(false);
   const statusLabel = useRef();
+  const sentOtpButtonRef = useRef();
+  const verifyOtpButtonRef = useRef();
   const otpBox = useRef();
-  const otpVerifyButtonRef = useRef();
 
+  const loaderRef = useRef();
+
+  // Things to do after OTP successfully verified
+  const otpVerifiedSuccessfully = () => {
+    /*
+    1. Notify user with notification
+    2. disable verify OTP button
+    */
+    setStatusLabel("Verified", "green");
+    verifyOtpButtonRef.current.disabled = true;
+  };
+
+  // Thins to do after OTP sent to the given number
+  const otpSentSuccessfully = () => {
+    /*
+      1. Notify user with notification
+      2. Disable sent OTP button
+      3. Visible the input field to input OTP
+      4. hide the loader
+
+    */
+
+    setStatusLabel("Check your mobile OTP has been sent!", "green");
+    sentOtpButtonRef.current.disabled = true;
+    otpBox.current.style.visibility = "visible";
+    setLoading(false);
+  };
+
+  // Utility method for notification
   const setStatusLabel = (msg, textColor = "red") => {
     statusLabel.current.style.color = textColor;
     statusLabel.current.innerHTML = `***${msg}***`;
@@ -26,18 +56,45 @@ const MobileNumberVerification = () => {
       .confirm(code)
       .then((result) => {
         // User signed in successfully.
-        const user = result.user;
-        console.log("Success");
-        verifyButtonRef.current.disabled = true;
+        console.log(result);
 
-        setStatusLabel("Verified", "green");
-        otpVerifyButtonRef.current.disabled = true;
-
-        // ...
+        otpVerifiedSuccessfully();
       })
       .catch((error) => {
         // User couldn't sign in (bad verification code?)
         setStatusLabel("Wrong OTP! Try again!");
+      });
+  };
+
+  const verifyNumber = (event) => {
+    event.preventDefault();
+    setUpRecaptcha();
+
+    setLoading(true);
+
+    const number = document.querySelector("#phone-num-inp").value;
+    const phoneNumber = `+91${number}`;
+    console.log(phoneNumber);
+
+    const appVerifier = window.recaptchaVerifier;
+    firebase
+      .auth()
+      .signInWithPhoneNumber(phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        window.confirmationResult = confirmationResult;
+        setConfirmationResult(confirmationResult);
+
+        otpSentSuccessfully();
+      })
+      .catch((error) => {
+        // Error; SMS not sent
+        setLoading(false);
+        sentOtpButtonRef.current.disabled = true;
+        if (!statusLabel.current.innerHTML.includes("Wrong OTP! Try again!")) {
+          setStatusLabel("Please refersh the page and try again!");
+        }
       });
   };
 
@@ -53,61 +110,30 @@ const MobileNumberVerification = () => {
     });
   };
 
-  const verifyNumber = (event) => {
-    event.preventDefault();
-    setUpRecaptcha();
-
-    const [phone] = event.target.elements;
-
-    const phoneNumber = phone.value;
-    console.log(phoneNumber);
-
-    const appVerifier = window.recaptchaVerifier;
-    firebase
-      .auth()
-      .signInWithPhoneNumber(phoneNumber, appVerifier)
-      .then((confirmationResult) => {
-        // SMS sent. Prompt user to type the code from the message, then sign the
-        // user in with confirmationResult.confirm(code).
-        window.confirmationResult = confirmationResult;
-        otpBox.current.style.visibility = "visible";
-        verifyButtonRef.current.disabled = true;
-
-        setStatusLabel("Check your mobile OTP has been sent!", "green");
-
-        setConfirmationResult(confirmationResult);
-      })
-      .catch((error) => {
-        // Error; SMS not sent
-        verifyButtonRef.current.disabled = true;
-        if (!statusLabel.current.innerHTML.includes("Wrong OTP! Try again!")) {
-          setStatusLabel("Please refersh the page and try again!");
-        }
-      });
-  };
-
   return (
     <div className="mobile-num-verification">
-      <form onSubmit={verifyNumber}>
+      <div>
         <div id="re-c"></div>
         <input
           type="tel"
-          id="phone"
+          id="phone-num-inp"
           name="phone"
-          placeholder="e.g. +918912198076"
-          maxLength="13"
+          placeholder="e.g. +91**********"
+          maxLength="10"
         />
 
-        <button type="submit" ref={verifyButtonRef}>
+        <button type="button" onClick={verifyNumber} ref={sentOtpButtonRef}>
           Sent OTP
         </button>
+        {isLoading && <div ref={loaderRef} className="loading"></div>}
+
         <div style={{ visibility: "hidden" }} className="otpBox" ref={otpBox}>
           <input id="otp-str" type="text" placeholder="Enter OTP" />
-          <button ref={otpVerifyButtonRef} onClick={verifyOTP}>
+          <button type="button" ref={verifyOtpButtonRef} onClick={verifyOTP}>
             Verify
           </button>
         </div>
-      </form>
+      </div>
       <p className="status-p" ref={statusLabel}></p>
     </div>
   );
