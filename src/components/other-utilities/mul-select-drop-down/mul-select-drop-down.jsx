be09a1aea89e4ee9d914c1daf5d4e4756a413f1b
prakhar7s <prakharshrivastava971@gmail.com>
2021-06-19 00:38:20 +0530

    dropdown menu in progress

--- src/components/other-utilities/mul-select-drop-down/mul-select-drop-down.jsx
+++ src/components/other-utilities/mul-select-drop-down/mul-select-drop-down.jsx
@@ -1,13 +1,84 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./mul-select-drop-down.css";
+import uuid from "uuid/v1";
+
+import CheckIcon from "@material-ui/icons/Check";
 
 const MulSelectDropDown = () => {
+  const [dropdownItems, setDropdownItems] = useState([
+    "Hello",
+    "Hi",
+    "Bye",
+    "Hello",
+    "Hi",
+    "Bye",
+    "Hello",
+    "Hi",
+    "Bye",
+    "Hello",
+    "Hi",
+    "Bye",
+  ]);
+
+  const [dropdownItemsDict, setDropdownItemsDict] = useState([]);
+
+  // preprocessing
+  /*
+    1. create dictionary of menu items
+    2.  
+
+  */
+  const preprocessing = () => {
+    setDropdownItemsDict(
+      dropdownItems.map((dropdownItem) => ({
+        id: uuid(),
+        itemName: dropdownItem,
+        isSelected: false,
+      }))
+    );
+  };
+
+  const dropdownItemClickHandler = (dropdownItem) => {
+    const clickItem = dropdownItemsDict.filter(
+      (item) => item.id === dropdownItem.id
+    )[0];
+
+    clickItem["isSelected"] = !dropdownItem.isSelected;
+
+    const allOther = dropdownItemsDict.filter(
+      (item) => item.id !== dropdownItem.id
+    );
+
+    setDropdownItemsDict([...allOther, clickItem]);
+  };
+
+  useEffect(() => {
+    preprocessing();
+  }, []);
   return (
     <>
       <h1 className="field-name">sdddasd</h1>
       <ul className="field-values">
-        <li>A</li>
+        <li className="nav-item has-dropdown">
+          <a href="#">A</a>
+          <ul className="dropdown">
+            {dropdownItemsDict.map((dropdownItem) => {
+              const { id, itemName, isSelected } = dropdownItem;
+              return (
+                <li key={id} className="dropdown-item">
+                  <a
+                    href="#"
+                    onClick={() => dropdownItemClickHandler(dropdownItem)}
+                  >
+                    {itemName}
+                  </a>
+                  {isSelected && <CheckIcon />}
+                </li>
+              );
+            })}
+          </ul>
+        </li>
       </ul>
     </>
   );
