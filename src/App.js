35a8a1a412b20b27928f11defc866c4fd39ab237
prakhar7s <prakharshrivastava971@gmail.com>
2021-06-21 20:59:50 +0530

    multiple select styles improved

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
-import MulSelectDropDown from "./components/other-utilities/mul-select-drop-down/mul-select-drop-down";
-// import WordSearch from "./components/other-utilities/word-search/word-search";
+// import MulSelectDropDown from "./components/other-utilities/mul-select-drop-down/mul-select-drop-down";
+import WordSearch from "./components/other-utilities/word-search/word-search";
 // import MobileNumberVerification from "./components/firebase-services/mobile-number-verification";
 
 function App() {
   return (
     <div className="App">
       {/* <MobileNumberVerification /> */}
-      {/* <WordSearch /> */}
-      <MulSelectDropDown
+      <WordSearch />
+      {/* <MulSelectDropDown
         dropdownName="cities"
         dropdownItems={["Mumbai", "Banglore", "Chennai", "Hydrabad"]}
         howManySelects={3}
-      />
+      /> */}
     </div>
   );
 }
